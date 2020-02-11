0ecb9aa1c2e69ae00320ff0c8d69f5f7cf230349
Jun Tan <tanjunyuan@hotmail.co.uk>
2020-02-11 14:43:07 -0500

    clean up notify-context path, add more tests

--- cpp/perspective/src/include/perspective/data_table.h
+++ cpp/perspective/src/include/perspective/data_table.h
@@ -56,11 +56,16 @@ public:
     PSP_NEW_DELETE(t_data_table)
 #endif
     PSP_NON_COPYABLE(t_data_table);
-    t_data_table(const t_schema& s, t_uindex capacity = DEFAULT_EMPTY_CAPACITY);
-    // Only use in tests, it inits the table unlike other constructors
-    t_data_table(const t_schema& s, const std::vector<std::vector<t_tscalar>>& v);
 
-    t_data_table(const std::string& name, const std::string& dirname, const t_schema& s,
+    t_data_table(
+        const t_schema& s, t_uindex capacity = DEFAULT_EMPTY_CAPACITY);
+
+    // Only use in tests, it inits the table unlike other constructors
+    t_data_table(
+        const t_schema& s, const std::vector<std::vector<t_tscalar>>& v);
+
+    t_data_table(
+        const std::string& name, const std::string& dirname, const t_schema& s,
         t_uindex init_cap, t_backing_store backing_store);
     ~t_data_table();
 
@@ -104,7 +109,8 @@ public:
     void clear();
     void reset();
 
-    t_mask filter_cpp(t_filter_op combiner, const std::vector<t_fterm>& fops) const;
+    t_mask filter_cpp(
+        t_filter_op combiner, const std::vector<t_fterm>& fops) const;
     t_data_table* clone_(const t_mask& mask) const;
     std::shared_ptr<t_data_table> clone(const t_mask& mask) const;
     std::shared_ptr<t_data_table> clone() const;
@@ -116,9 +122,11 @@ public:
      * 
      * @return std::shared_ptr<t_data_table> 
      */
-    std::shared_ptr<t_data_table> borrow(const std::vector<std::string>& columns) const;
+    std::shared_ptr<t_data_table> borrow(
+        const std::vector<std::string>& columns) const;
 
-    t_column* clone_column(const std::string& existing_col, const std::string& new_colname);
+    t_column* clone_column(
+        const std::string& existing_col, const std::string& new_colname);
 
     std::vector<const t_column*> get_const_columns() const;
     std::vector<t_column*> get_columns();
@@ -126,10 +134,18 @@ public:
     void set_column(t_uindex idx, std::shared_ptr<t_column> col);
     void set_column(const std::string& name, std::shared_ptr<t_column> col);
 
-    std::shared_ptr<t_column> add_column_sptr(const std::string& cname, t_dtype dtype, bool status_enabled);
+    std::shared_ptr<t_column> add_column_sptr(
+        const std::string& cname, t_dtype dtype, bool status_enabled);
 
-    t_column* add_column(const std::string& cname, t_dtype dtype, bool status_enabled);
+    t_column* add_column(
+        const std::string& cname, t_dtype dtype, bool status_enabled);
 
+    /**
+     * @brief Given a column name, clear the underlying column but do not
+     * mutate the `t_data_table` instance's `m_schema` and `m_columns`.
+     * 
+     * @param name 
+     */
     void drop_column(const std::string& name);
 
     void promote_column(
